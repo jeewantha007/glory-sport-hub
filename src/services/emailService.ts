4d2cde576a645520468b2595e4fa2c2831cc2632
jeewantha sandeepa <hadjeewantha@gmail.com>
2025-10-12 22:10:09 +0530

    push

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/integrations/supabase/client";
 
 export const emailService = {
-  // Subscribe to newsletter
+
   async subscribeToNewsletter(name: string, email: string) {
     const { error } = await supabase
       .from("email_subscribers")
